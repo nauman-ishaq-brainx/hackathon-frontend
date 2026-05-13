61301c7326eeb1350ffa75be7847fbc14f162237
Nauman Ishaq <nauman.ishaq@brainxtech.com>
2026-05-13 15:28:41 +0500

    added boiler plate code

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'system-ui, sans-serif' }}>
+      <h1>Hackathon Frontend</h1>
+      <p>Vite + React is running.</p>
+    </main>
+  )
+}
+
+export default App
